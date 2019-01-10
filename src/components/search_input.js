638568677ddfecdfe603e5cba1259f98d3984418
Александр <34546109+maxshow1970@users.noreply.github.com>
2019-01-10 11:00:28 +0200

    Update search_input.js

--- src/components/search_input.js
+++ src/components/search_input.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react'
 
 
 export default class SearchInput extends Component {
-    // constructor(props) {
-    //     super(props);
-    // }
 
     onInputChange(term) {
         this.props.onSearchTermChange(term);
