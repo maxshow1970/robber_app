f8957c4061619a4a3676c6492fd53ee9ec6dbb60
Александр <34546109+maxshow1970@users.noreply.github.com>
2019-01-10 10:55:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import Popover from 'material-ui/Popover'
 import {AppForm} from './components/form'
 import axios from "axios"
 import  SortByButtons   from './components/sort_buttons'
+import  SortKnByButtons   from './components/sort_kn_buttons'
 import SearchInput from './components/search_input'
 import KnapsackInput from './components/knapsack_input'
 import {KnackpackList} from './components/knackpack_list'
 import {KnackpackButtons} from './components/knackpack_button'
-import  LabelSum  from './components/labelsum'
+
+// import  LabelSum  from './components/labelsum'
 
 
 export default class App extends Component {
@@ -22,7 +24,7 @@ export default class App extends Component {
       isDrawerOpen: false,
       isPopoverOpen: false,
       popoverElement: null,
-      magazinList: [],
+      marketList: [],
       Searchnames: [],
       sizeKnapsack: 100,
       knapsackList: [],
@@ -34,10 +36,10 @@ export default class App extends Component {
 
   componentDidMount() {
         axios
-            .get("http://localhost:3000/magazin")
+            .get("http://localhost:3000/market")
             .then(response => {
 
-                const newMagazin = response.data.map(c => {
+                const newMarket = response.data.map(c => {
                     return {
                         id: c.id,
                         name: c.name,
@@ -47,8 +49,8 @@ export default class App extends Component {
                 });
 
                 const newState = Object.assign({}, this.state, {
-                    magazinList: newMagazin,
-                    Searchnames:  newMagazin
+                    marketList: newMarket,
+                    Searchnames:  newMarket
                 });
 
                 this.setState(newState);
@@ -60,22 +62,22 @@ export default class App extends Component {
   }
 
   onAdd(item){
-        let magazinList= this.state.magazinList
-        magazinList.push(item)
-        this.setState({magazinList, isPopoverOpen: false})
+        let marketList= this.state.marketList
+        marketList.push(item)
+        this.setState({marketList, isPopoverOpen: false})
   }
 
     changeList(prop) {
         var term = prop['prop'].toLowerCase(),
-            magazinList = this.state.Searchnames,
+            marketList = this.state.Searchnames,
             newList = [];
 
-        for (var i = 0; i < magazinList.length; i++) {
-            if (magazinList[i]['name'].toLowerCase().indexOf(term) > -1) {
-                newList.push(magazinList[i]);
+        for (var i = 0; i < marketList.length; i++) {
+            if (marketList[i]['name'].toLowerCase().indexOf(term) > -1) {
+                newList.push(marketList[i]);
             }
         }
-        this.setState({magazinList: newList});
+        this.setState({marketList: newList});
     }
 
     changeKnackpack(prop) {
@@ -85,7 +87,7 @@ export default class App extends Component {
 
 
     sortList(prop) {
-        var dataObj = this.state.magazinList;
+        var dataObj = this.state.marketList;
 
         // sort by name
         if(prop.prop.type === 'nameASC'){
@@ -121,7 +123,47 @@ export default class App extends Component {
                 });
             }
         }
-        this.setState({magazinList: dataObj});
+        this.setState({marketList: dataObj});
+    }
+
+    sortKnList(prop) {
+        var dataObj = this.state.knapsackList;
+
+        // sort by name
+        if(prop.prop.type === 'nameASC'){
+            if(prop.prop.term === true){
+                dataObj.sort(function(a, b) {
+                    var nameA = a.name.toUpperCase();
+                    var nameB = b.name.toUpperCase();
+                    return (nameA < nameB) ? -1 : (nameA > nameB) ? 1 : 0;
+                });
+            }
+            else{
+                dataObj.sort(function(a, b) {
+                    var nameA = a.name.toUpperCase();
+                    var nameB = b.name.toUpperCase();
+                    return (nameA > nameB) ? -1 : (nameA < nameB) ? 1 : 0;
+                });
+            }
+        }
+        else{
+            // sort by size
+            if(prop.prop.term === true){
+                dataObj.sort(function(a, b) {
+                    var sizeA = a.size;
+                    var sizeB = b.size;
+                    return (sizeA < sizeB) ? -1 : (sizeA > sizeB) ? 1 : 0;
+                });
+            }
+            else{
+                dataObj.sort(function(a, b) {
+                    var sizeA = a.size;
+                    var sizeB = b.size;
+                    return (sizeA > sizeB) ? -1 : (sizeA < sizeB) ? 1 : 0;
+                });
+            }
+        }
+        this.setState({knapsackList: dataObj});
     }
 
   render() {
@@ -136,16 +178,16 @@ export default class App extends Component {
          />
       <div className="main">
 
-        <div className="containerMagazin">
-            <h1>Magazin</h1>
+        <div className="containerMarket">
+            <h1>Market</h1>
 
             <SearchInput onSearchTermChange={prop => this.changeList({prop})}/>
             <SortByButtons sortList={prop => this.sortList({prop})}/>
            <AppButtons
-              itemsChecked = {this.state.magazinList.map(i => i.checked).filter(i => i)}
+              itemsChecked = {this.state.marketList.map(i => i.checked).filter(i => i)}
               onDelete = {() => {
-                let magazinList = this.state.magazinList.filter(i => !i.checked)
-                this.setState({ magazinList })
+                let marketList = this.state.marketList.filter(i => !i.checked)
+                this.setState({ marketList })
               }}
               onAdd = {(popoverElement) =>{this.setState({
                   popoverElement,
@@ -167,19 +209,12 @@ export default class App extends Component {
                           } else {
                               flag = false
                           }
-                              // if (item.name in  knapsackList.name  ){
-                              //     flag = false;
-                              //     // break;
-                              // } else {
-                              //     flag = true
-                              // }
-                          // }
                       }
                       return flag;
                   }
 
 
-                  let knapsackList_temp = this.state.magazinList.filter(i => i.checked).slice()
+                  let knapsackList_temp = this.state.marketList.filter(i => i.checked).slice()
                   let knapsackList = this.state.knapsackList
 
                   for (var i = 0; i < knapsackList_temp.length; i++) {
@@ -216,11 +251,11 @@ export default class App extends Component {
               />
           </Popover>
            <AppList
-              items = {this.state.magazinList}
+              items = {this.state.marketList}
               onCheck={(checked, idx) => {
-                let {magazinList} = this.state
-                magazinList[idx].checked = checked
-                this.setState({magazinList})
+                let {marketList} = this.state
+                marketList[idx].checked = checked
+                this.setState({marketList})
               }}
            />
         </div>
@@ -228,13 +263,7 @@ export default class App extends Component {
              <h1>Knapsack</h1>
             <KnapsackInput onInputPackChange={prop => this.changeKnackpack({prop})}/>
 
-            <SortByButtons sortList={prop => this.sortList({prop})}/>
-            <LabelSum
-                selectedSum = {this.state.selectedSum}
-                sizeKnapsack = { this.state.sizeKnapsack}
-
-
-            />
+            <SortKnByButtons sortKnList={prop => this.sortKnList({prop})}/>
 
             <KnackpackButtons
                 itemsKChecked = {this.state.knapsackList.map(i => i.checked).filter(i => i)}
