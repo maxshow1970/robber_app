4e4d796bb25311d1bb08513ea62cb3c71fe6998d
Александр <34546109+maxshow1970@users.noreply.github.com>
2019-01-10 10:59:59 +0200

    Update knapsack_input.js

--- src/components/knapsack_input.js
+++ src/components/knapsack_input.js
@@ -3,10 +3,6 @@ import TextField from 'material-ui/TextField'
 
 
 export default class KnapsackInput extends Component {
-    // constructor(props) {
-    //     super(props);
-    // }
-
 
     onInputChange(event) {
         var val = event.target.value;
@@ -26,18 +22,9 @@ export default class KnapsackInput extends Component {
                     hintText="Content"
                     floatingLabelText="Enter the capacity of the knapsack"
                     fullWidth={true}
-
-                    // defaultValue={ this.state.sizeKnapsack}
-                    // errorText={this.state.errors.description}
-                    // onChange={(event, sizeKnapsack) => this.setState({sizeKnapsack})}
-                  //  onChange={ event => this.onInputChange(event.target.value)}
                     onChange={ event => this.onInputChange(event)}
 
                 />
-                {/*<input className="form-control"*/}
-                       {/*type="text"*/}
-                       {/*placeholder="Search by name..."*/}
-                       {/*onChange={ event => this.onInputChange(event.target.value)} />*/}
             </div>
         )
     }
