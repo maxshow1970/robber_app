4aee7c0a94c48605d8aee4f71562fd7bde8a1e64
Александр <34546109+maxshow1970@users.noreply.github.com>
2019-01-10 10:58:14 +0200

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,40 +1,13 @@
  import React from 'react'
  import AppBar from 'material-ui/AppBar'
- import {  indigo700, grey50 } from "material-ui/styles/colors"
- import IconMenu from 'material-ui/IconMenu'
- import MenuItem from 'material-ui/MenuItem'
- import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
- import IconButton from  'material-ui/IconButton'
-
-
-
- const RightMenu = () => (
-     <IconMenu
-         iconButtonElement={
-             <IconButton>
-                 <MoreVertIcon color = { grey50 }/>
-             </IconButton>
-         }
-         targetOrigin={{horizontal: 'right', vertical: 'top'}}
-         anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-     >
-         <MenuItem primaryText="Refresh" />
-         <MenuItem primaryText="Help" />
-         <MenuItem primaryText="Sign out" />
-     </IconMenu>
- );
-
+ import {  indigo700 } from "material-ui/styles/colors"
 
  export const AppHeader = (props) => {
     return(
-
         <AppBar
             title="Robber App"
-            // iconClassNameRight="muidocs-icon-navigation-expand-more"
             style ={{background : indigo700}}
-            iconElementRight={<RightMenu />}
-            onLeftIconButtonClick = { () => props.onLeftIconClick( )}
-
+             onLeftIconButtonClick = { () => props.onLeftIconClick( )}
         />
     )
  }
